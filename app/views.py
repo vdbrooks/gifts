29a58bfe68e658d5dc7b38e4028aeca9159088c0
root <vitorrio.brooks@rightbrainnetworks.com>
2016-04-11 12:00:15 -0400

    removing flush, since it's called by default anyway

--- app/views.py
+++ app/views.py
@@ -14,7 +14,7 @@ def signup():
             db.session.commit()
         except Exception as e:
             db.session.rollback()
-            db.session.flush()
+            #db.session.flush()
             return 'Username already in use'
         return "Storing new user: {}".format(request.form['email'])
 
