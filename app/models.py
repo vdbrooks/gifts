8e3ed2b032823bd022f0ee0826e3b336291da091
root <vitorrio.brooks@rightbrainnetworks.com>
2016-04-11 22:52:22 -0400

    updating user model to hash passwords

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -6,11 +7,19 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(320), unique=False)
     giftees = db.relationship('Giftee', backref='gifter', lazy='dynamic')
+    password_hash = db.Column(db.String(128))
 
-    #def __init__(self, username, email, password):
-    #    self.username = username
-    #    self.email = email
-    #    self.password = password
+    @property
+    def password(self):
+        raise AttributeError('Bro, you\'re not allowed to read plain text passwords')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password, method='pbkdf2:sha512',salt_length=64)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     def __repr__(self):
         return '<User %r>' % (self.username)
 
