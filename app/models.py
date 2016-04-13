1c4469aaec43e62542c1ca56feb402e564f7539c
root <vitorrio.brooks@rightbrainnetworks.com>
2016-04-12 22:41:53 -0400

    removed password field, replaced with password_hash

--- app/models.py
+++ app/models.py
@@ -5,7 +5,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(320), unique=False)
     giftees = db.relationship('Giftee', backref='gifter', lazy='dynamic')
     password_hash = db.Column(db.String(128))
 
